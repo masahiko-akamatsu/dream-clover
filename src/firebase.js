7303de8c42480f198a8cef79b406dd6f9b3bf328
masahiko-akamatsu <a.nakin.skywk@icloud.com>
2026-04-15 11:18:42 +0800

    add src/firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2yIV5R5tMkWlmQLT2Cxb_MnWiQZhVGSk",
+  apiKey: "AIzaSyC64BmjAX5V8ZGnU-kLOQpRzrS98RoRE-8",
   authDomain: "yosyukupro.firebaseapp.com",
   projectId: "yosyukupro",
-  storageBucket: "yosyukupro.appspot.com",
-  messagingSenderId: "459825852657",
-  appId: "1:459825852657:web:b2e7a3c8d4f1e2a3b5c6d7"
+  storageBucket: "yosyukupro.firebasestorage.app",
+  messagingSenderId: "824661488718",
+  appId: "1:824661488718:web:f4951ee0e7c99b2e7a3f1f"
 };
 
 const app = initializeApp(firebaseConfig);
